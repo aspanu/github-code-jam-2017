bd5edcd9e9b0fbdb09928e705559436371ca37df
Adrian Spanu <adri@nspanu.ca>
2017-04-09 15:10:46 +0200

    Solution for Tidy problem b of 2017.

--- src/main/java/org/aspanu/codejam2017/tidyproblem/InputIngest.java
+++ src/main/java/org/aspanu/codejam2017/tidyproblem/InputIngest.java
@@ -1,5 +1,9 @@
 package org.aspanu.codejam2017.tidyproblem;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -11,11 +15,42 @@ public class InputIngest {
     Scanner scanner;
     Integer numProbs;
     List<Long> inputNumbers;
+    private static final String DIRECTORY = "/Users/aspanu/Downloads/";
+    private static final String FILE = "B-large-practice.in";
+    private static final String OUTPUT_FILE = "tidy_solution.out";
+
+    public static void main(String[] varargs) throws FileNotFoundException, UnsupportedEncodingException {
+        String fileInput = DIRECTORY + FILE;
+        String fileOutput = DIRECTORY + OUTPUT_FILE;
+        File input = new File(fileInput);
+        InputIngest inputIngest = new InputIngest(input);
+
+        PrintWriter output = new PrintWriter(fileOutput, "UTF-8");
+        inputIngest.solveTidyProblem(output);
+        output.close();
+    }
 
     public InputIngest() {
         scanner = new Scanner(System.in);
         numProbs = -1;
         inputNumbers = new ArrayList<Long>();
+    }
+
+    public InputIngest(File inputFile) throws FileNotFoundException {
+        scanner = new Scanner(inputFile);
+        numProbs = -1;
+        inputNumbers = new ArrayList<Long>(100);
+        init();
+    }
+
+    public void solveTidyProblem(PrintWriter output) {
+        TidyProblem tidy = new TidyProblem();
+        int c = 1;
+        for (Long number : inputNumbers) {
+            String payload = "Case #" + c + ": " + tidy.previousTidyNumberIntelligent(number);
+            output.println(payload);
+            c++;
+        }
     }
 
     public void init() {
--- src/test/java/org/aspanu/codejam2017/tidyproblem/TidyProblemTest.java
+++ src/test/java/org/aspanu/codejam2017/tidyproblem/TidyProblemTest.java
@@ -1,5 +1,6 @@
 package org.aspanu.codejam2017.tidyproblem;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -10,9 +11,55 @@ import static org.hamcrest.core.IsEqual.equalTo;
  */
 public class TidyProblemTest {
 
+
+    private TidyProblem tidy;
+
+    @Before
+    public void setUp() {
+        tidy = new TidyProblem();
+    }
+
     @Test
     public void testIsTidyNumber() {
-        TidyProblem tidy = new TidyProblem();
-        assertThat(tidy.isTidyNumber(123), equalTo(true));
+        assertThat(tidy.isTidyNumber(123L), equalTo(true));
+        assertThat(tidy.isTidyNumber(10L), equalTo(false));
+        assertThat(tidy.isTidyNumber(132L), equalTo(false));
+        assertThat(tidy.isTidyNumber(7L), equalTo(true));
+        assertThat(tidy.isTidyNumber(99999999999999999L), equalTo(true));
     }
+
+    @Test
+    public void testPreviousTidyNumberNaive() {
+        assertThat(tidy.previousTidyNumberNaive(132L), equalTo(129L));
+        assertThat(tidy.previousTidyNumberNaive(1000L), equalTo(999L));
+        assertThat(tidy.previousTidyNumberNaive(7L), equalTo(7L));
+        // This takes too long for the naive implementation
+        //assertThat(tidy.previousTidyNumberNaive(111111111111111110L), equalTo(99999999999999999L));
+    }
+
+    @Test
+    public void testGetUntidyIndex() {
+        assertThat(tidy.getUntidyIndex(7L), equalTo(-1));
+        assertThat(tidy.getUntidyIndex(132L), equalTo(1));
+        assertThat(tidy.getUntidyIndex(1000L), equalTo(0));
+    }
+
+    @Test
+    public void testLowerDigitAndCascadeNumber() {
+        assertThat(tidy.lowerDigitAndCascadeNumber(10L, 0), equalTo(9L));
+        assertThat(tidy.lowerDigitAndCascadeNumber(20L, 0), equalTo(19L));
+        assertThat(tidy.lowerDigitAndCascadeNumber(1066L, 1), equalTo(999L));
+        assertThat(tidy.lowerDigitAndCascadeNumber(10066L, 2), equalTo(9999L));
+    }
+
+    @Test
+    public void testPreviousTidyNumberIntelligent() {
+        TidyProblem tidyProblem = new TidyProblem();
+        assertThat(tidyProblem.previousTidyNumberIntelligent(132L), equalTo(129L));
+        assertThat(tidyProblem.previousTidyNumberIntelligent(1000L), equalTo(999L));
+        assertThat(tidyProblem.previousTidyNumberIntelligent(7L), equalTo(7L));
+        assertThat(tidyProblem.previousTidyNumberIntelligent(111111111111111110L), equalTo(99999999999999999L));
+    }
+
+
 }
